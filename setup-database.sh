bbc892e712775207b0b9da3aba01dc97266e819a
AshutoshRaval <122703876+AshutoshRaval@users.noreply.github.com>
2023-10-17 18:42:55 -0400

    updated database sh file

--- setup-database.sh
+++ setup-database.sh
@@ -65,10 +65,14 @@ if sudo mysql -u root -e "USE $DB_NAME" 2>/dev/null; then
     echo "Database $DB_NAME already exists."
 else
     echo "Creating database $DB_NAME..."
-    sudo mysql -u root -p root -e "CREATE DATABASE $DB_NAME;"
-    sudo mysql -u root -p root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'localhost' IDENTIFIED BY 'root';"
-    sudo mysql -u root -p root -e "FLUSH PRIVILEGES;"
+    sudo mysql -u root -proot -e "CREATE DATABASE $DB_NAME;"
+    sudo mysql -u root -proot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'localhost' IDENTIFIED BY 'root';"
+    sudo mysql -u root -proot -e "FLUSH PRIVILEGES;"
+    
 
     echo "Database $DB_NAME created."
 fi
 
+if sudo mysql -u root -e "USE $DB_NAME" 2>/dev/null; then
+    echo "Database $DB_NAME already exists."
+fi

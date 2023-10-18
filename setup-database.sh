73ab802b2660e1a753543d5f7a366bff5ade0178
AshutoshRaval <122703876+AshutoshRaval@users.noreply.github.com>
2023-10-18 13:02:02 -0400

    updated bash

--- setup-database.sh
+++ setup-database.sh
@@ -1,42 +1,11 @@
-# #!/bin/bash
-
-# # Update and upgrade the system
-# sudo apt update && sudo apt -y upgrade
-
-# # Install Node.js, npm, MariaDB server, and client
-# sudo apt -y install nodejs npm mariadb-server mariadb-client
-
-# # Start and enable MariaDB
-# sudo systemctl start mariadb
-# sudo systemctl enable mariadb
-
-# # # Secure the MariaDB installation
-# # # Note: In production, utilize a more secure way of handling MySQL root password 
-# # # such as using secret management tools or AWS Secrets Manager.
-# # sudo mysql_secure_installation <<EOF
-# # y
-# # [YourRootPassword]
-# # [YourRootPassword]
-# # y
-# # y
-# # y
-# # y
-# # EOF
-
-# # Setup database and user for your web app
-# # Remember to replace [YourRootPassword] and [YourDBName] accordingly.
-
-# sudo mysql -u  <<EOF
-# CREATE DATABASE TestDataBase1;
-# GRANT ALL PRIVILEGES ON TestDataBase1.* TO 'root'@'localhost' IDENTIFIED BY 'root';
-# FLUSH PRIVILEGES;
-# EXIT;
-# EOF
-
-# # You may further install and configure your web application below this line
-
-
 #!/bin/bash
+
+if [ -f .env ]; then
+  source .env
+else
+  echo ".env file not found. Make sure to create it."
+  exit 1
+fi
 
 # Update and upgrade the system
 sudo apt update && sudo apt -y upgrade
@@ -65,10 +34,10 @@ if sudo mysql -u root -e "USE $DB_NAME" 2>/dev/null; then
     echo "Database $DB_NAME already exists."
 else
     echo "Creating database $DB_NAME..."
-    sudo mysql -u root -proot -e "CREATE DATABASE $DB_NAME;"
-    sudo mysql -u root -proot -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'localhost' IDENTIFIED BY 'root';"
-    sudo mysql -u root -proot -e "FLUSH PRIVILEGES;"
-    sudo mysql -u root -proot -e "SHOW DATABASES;"
+    sudo mysql -u root -p"$DB_PASSWORD" -e "CREATE DATABASE $DB_NAME;"
+    sudo mysql -u root -p"$DB_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+    sudo mysql -u root -p"$DB_PASSWORD" -e "FLUSH PRIVILEGES;"
+    sudo mysql -u root -p"$DB_PASSWORD" -e "SHOW DATABASES;"
     echo "Database $DB_NAME created."
 
 fi

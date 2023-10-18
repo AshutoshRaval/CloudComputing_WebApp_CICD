9d95569f13a9d5f2d87d9ea3c0faa588dccd5486
AshutoshRaval <122703876+AshutoshRaval@users.noreply.github.com>
2023-10-18 12:48:01 -0400

    updated local host (#26)
    * Commit 1
    
    * updated test
    
    * removed old packer
    
    * updated the packer-ci
    
    * modified the yml file
    
    * updating the yml file
    
    * create ci merge
    
    * updated packer-ci-merge
    
    * updated merge file
    
    * updated yml
    
    * updated packer.hcl
    
    * updated bash file
    
    * updated yml
    
    * updated packer hcl
    
    * updated hcl
    
    * Update aws-debian.pkr.hcl
    
    * updated packer file
    
    * updated packer
    
    * updated packer
    
    * updated hcl with fmt
    
    * updated packer file
    
    * updated packer
    
    * updated hcl
    
    * updated hcl
    
    * updated bash file
    
    * Updated the packer file
    
    * updated bash
    
    * updated bash
    
    * updated packer.hcl
    
    * updated database sh file
    
    * Update aws-debian.pkr.hcl
    
    * updated packer hcl
    
    * Updated packer hcl
    
    * Updated hcl and ci merge
    
    * Updated ci merege
    
    * Updated files
    
    * Updated packer
    
    * formated packer
    
    * updated local host

--- Models/db.js
+++ Models/db.js
@@ -28,7 +28,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     // dialect: 'mysql',
     dialect: 'mariadb',
-    host:'localhost',
+    host:'127.0.0.1',
     port: 3306,
 
 });

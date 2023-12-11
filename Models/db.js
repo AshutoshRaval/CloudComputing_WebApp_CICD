351e78e77483ad55afa4222734e9e16acd9c88b5
AshutoshRaval <122703876+AshutoshRaval@users.noreply.github.com>
2023-12-10 20:34:24 -0500

    Updated Code with the Readme File (#91)
    * Commit 1
    
    * updated test
    
    * removed old packer
    
    * updated the packer-ci
    
    * modified the yml file
    
    * updating the yml file
    
    * create ci merge
    
    * updated packer-ci-merge
    
    * updated merge file
    
    * updated yml
    
    * updated packer.hcl
    
    * updated bash file
    
    * updated yml
    
    * updated packer hcl
    
    * updated hcl
    
    * Update aws-debian.pkr.hcl
    
    * updated packer file
    
    * updated packer
    
    * updated packer
    
    * updated hcl with fmt
    
    * updated packer file
    
    * updated packer
    
    * updated hcl
    
    * updated hcl
    
    * updated bash file
    
    * Updated the packer file
    
    * updated bash
    
    * updated bash
    
    * updated packer.hcl
    
    * updated database sh file
    
    * Update aws-debian.pkr.hcl
    
    * updated packer hcl
    
    * Updated packer hcl
    
    * Updated hcl and ci merge
    
    * Updated ci merege
    
    * Updated files
    
    * Updated packer
    
    * formated packer
    
    * updated local host
    
    * updated bash
    
    * updated bash and hcl file
    
    * formated hcl
    
    * updated api and basic auth
    
    * upadated code
    
    * updated env
    
    * updated bash
    
    * updated bash
    
    * updated hcl file
    
    * Updated some files, added webasp.service file
    
    * updated packer file
    
    * updated packer file
    
    * updated bash
    
    * Updated env file
    
    * Updated DB.js
    
    * updated the bash file
    
    * Updated service fille
    
    * updated webapp.service file
    
    * Modified server.js
    
    * updated bash file
    
    * updated packer file
    
    * updated bash
    
    * updated  bash
    
    * updated bash
    
    * updated bash
    
    * updated bash
    
    * updated webapp.service
    
    * Update aws-debian.pkr.hcl
    
    * updated packer
    
    * updated bash
    
    * updated webapp.service
    
    * Updated webapp
    
    * Updated code
    
    * Updated packge.json
    
    * Updated the Code
    
    * Update the Packer file
    
    * Updated yaml file
    
    * Updated the yaml
    
    * Updated Code in Yaml file
    
    * Update packer-ci-merge.yml
    
    * Update app.js
    
    * updated the webapp
    
    * Updated Yaml
    
    * Updated YML
    
    * Updated the packer file
    
    * Updated packer
    
    * Updated yml
    
    * upadted
    
    * updatd code
    
    * Updated chnags
    
    * Updated code
    
    * Update packer-ci-LT.yml
    
    * Updated code
    
    * Updaed
    
    * Update packer-ci-LT.yml
    
    * Update packer-ci-LT.yml
    
    * Updated code
    
    * Updated code
    
    * Update packer-ci-merge.yml
    
    * Updated Code
    
    * Updated Code
    
    * Update packer-ci-Bot.yml
    
    * Updated the secret key
    
    * Updated code
    
    * Updated code sect
    
    * Updatd code
    
    * Updated code
    
    * Updated code
    
    * Ci merge Updated
    
    * Updated code
    
    * Updated the code
    
    * Updated code
    
    * Update packer-ci-Bot.yml
    
    * Update packer-ci-merge.yml
    
    * Updated Readme
    
    * Update aws-debian.pkr.hcl
    
    * Update aws-debian.pkr.hcl
    
    * Update processUsers.js
    
    * Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,27 +1,3 @@
-
-
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//     //   connectionLimit: 10,
-//     //   host: 'localhost',
-//     //   user: 'root',
-//     //   password: 'root123',
-//     //   database: 'TestDataBase1'
-//         connectionLimit: 10,
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_NAME
-//     });
-    
-// pool.on('error', (err) => {
-//       console.error('Database connection error:', err);
-//     });
-    
-// module.exports = pool;
-
 const Sequelize = require('sequelize'); 
 // Load dotenv only in development environment
 if (process.env.NODE_ENV !== 'production') {
--- Utils/processUsers.js
+++ Utils/processUsers.js
@@ -2,34 +2,9 @@
 const bcrypt = require('bcrypt');
 const sequelize = require('../Models/db');
 const User1 = sequelize.models.User;
-
-// async function processUsers(users) {
-//     for (let user of users) {
-//         // Hash the password before saving to the database
-//         const hashedPassword = await bcrypt.hash(user.password, 10);
-
-//         await User1.findOrCreate({
-//             where: { email: user.email },
-//             defaults: {
-//                 first_name: user.first_name,
-//                 last_name: user.last_name,
-//                 password: hashedPassword,
-//                 account_created: new Date(),
-//                 account_updated: new Date()
-//             }
-//         });
-//     }
-// }
-
-// module.exports = processUsers;
-
-// const bcrypt = require('bcrypt');
-// const { User } = require('../Models/user');
-
 const processUsers = async (users) => {
 
     console.log('In Process Users')
-    // console.log(users)
     for (let userEntry  of users) {
 
         let userData = userEntry ['first_name,last_name,email,password'].split(',');
@@ -39,23 +14,9 @@ const processUsers = async (users) => {
             email: userData[2],
             password: userData[3]
         };
-        // console.log(user.email)
-        // console.log(user.password)
-        // Hashing the password
-        // user.password = bcrypt.hashSync(userData[3], 10);
-
-        // var email1 = "ashutoshraval01@gmail.com"
-        // console.log(sequelize.models.User)
 
         const attributes = sequelize.models.User.rawAttributes;
         const fields = Object.keys(attributes);
-        // console.log(fields);
-        // console.log(User1.f)
-
-        
-
-        // Check if user exists in DB
-        // const existingUser = await User1.findOne({where: { user.email }});
         const existingUser = await  sequelize.models.User.findOne({ where: { email: user.email } });
 
         if (!existingUser) {
